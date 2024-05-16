591ef1f6d315713742ec0eb1909570a9c5ab267c
Gleb Trusov <gleb.trusow@yandex.ru>
2024-05-16 09:57:26 +0300

    [Terminal] Пунктирные линии

--- app/src/main/java/com/github/gltrusov/compose/samples/terminal/presentation/Terminal.kt
+++ app/src/main/java/com/github/gltrusov/compose/samples/terminal/presentation/Terminal.kt
@@ -5,14 +5,18 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.gestures.TransformableState
 import androidx.compose.foundation.gestures.transformable
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.PathEffect
+import androidx.compose.ui.graphics.drawscope.DrawScope
 import androidx.compose.ui.graphics.drawscope.translate
 import androidx.compose.ui.layout.onSizeChanged
+import androidx.compose.ui.unit.dp
 import com.github.gltrusov.compose.samples.terminal.data.Bar
 import kotlin.math.roundToInt
 
@@ -40,6 +44,7 @@ fun Terminal(bars: List<Bar>) {
         modifier = Modifier
             .fillMaxSize()
             .background(Color.Black)
+            .padding(vertical = 32.dp)
             .transformable(transformableState)
             .onSizeChanged { size ->
                 state = state.copy(terminalWidth = size.width.toFloat())
@@ -68,6 +73,59 @@ fun Terminal(bars: List<Bar>) {
                 )
             }
         }
+        state.visibleBars.firstOrNull()?.let {
+            drawPrices(
+                min = min,
+                pxPerPoint = pxPerPoint,
+                lastPrice = it.close
+            )
+        }
+
 
     }
 }
+
+private fun DrawScope.drawPrices(
+    min: Float,
+    pxPerPoint: Float,
+    lastPrice: Float
+) {
+    val dashPathEffect = PathEffect.dashPathEffect(
+        intervals = floatArrayOf(4.dp.toPx(), 4.dp.toPx())
+    )
+
+    //max
+    drawLine(
+        color = Color.White,
+        start = Offset(0f,0f),
+        end = Offset(size.width, 0f),
+        strokeWidth = 2f,
+        pathEffect = dashPathEffect
+    )
+
+    //last price
+    drawLine(
+        color = Color.White,
+        start = Offset(0f,size.height - ((lastPrice - min) * pxPerPoint)),
+        end = Offset(size.width, size.height - ((lastPrice - min) * pxPerPoint)),
+        strokeWidth = 2f,
+        pathEffect = dashPathEffect
+    )
+
+    //min
+    drawLine(
+        color = Color.White,
+        start = Offset(0f,size.height),
+        end = Offset(size.width, size.height),
+        strokeWidth = 2f,
+        pathEffect = dashPathEffect
+    )
+}
+
+
+
+
+
+
+
+
